4887a360ec495ad53de2de7ac896ff5f52215810
viswa475 <50315376+viswa475@users.noreply.github.com>
2020-01-03 21:33:23 +0530

    Add files via upload

---
+++ J_2/arrayReverse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,a[1000];
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<"Reversed Array:"<<"\n";
+	for(int j=n-1;j>=0;j--)
+	{
+		cout<<a[j]<<"\t";
+	}
+}
