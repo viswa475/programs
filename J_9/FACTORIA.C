c3112d763908e3431fb9c201f4a43331cffcb78d
viswa475 <50315376+viswa475@users.noreply.github.com>
2020-01-14 06:56:56 -0800

    Add files via upload

---
+++ J_9/FACTORIA.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+int fact (int n);
+void main()
+{
+	printf("enter the number");
+	scanf("%d",&n);
+	c=fact(n);
+	printf(factorial of %d is %d)
+	getch();
+}
+int fact
