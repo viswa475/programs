34a73afafcaf69951b565a83e34da90e924e406d
viswa475 <50315376+viswa475@users.noreply.github.com>
2020-01-15 06:13:38 -0800

    Add files via upload

---
+++ J_10/temperature.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float c,f;
+	cout<<"enter the temperature in farenheit:";
+	cin>>f;
+	c=5*(f-32)/9;
+	cout<<"temperature in celcius="<<c;
+	return 0;
+}
