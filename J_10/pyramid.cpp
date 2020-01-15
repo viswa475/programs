8f4f15e6de2ec736da7141154482b9bd180b9bba
viswa475 <50315376+viswa475@users.noreply.github.com>
2020-01-15 06:12:32 -0800

    Add files via upload

---
+++ J_10/pyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,k;
+	for(i=1;i<=5;i++)
+	{
+		k=1;
+		for(j=1;j<=i;j++)
+		{
+			cout<<k;
+			k++;
+			
+		}
+	      //k++;
+		
+		cout<<"\n";
+	}
+	return 0;
+}
